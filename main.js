378dd5a0b87f01c2110b06f695f12f119c619da5
jun <yongyong5766@gmail.com>
2020-12-30 12:36:14 +0900

    Prettier 적용

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 'use strict';
 // JSON 파일에서 항목 가져오기
 function loadItems() {
-    return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);    
+  return fetch('data/data.json')
+    .then((response) => response.json())
+    .then((json) => json.items);
 }
 
 // 지정된 항목으로 목록 업데이트
 function displayItems(items) {
-    const container = document.querySelector('.items');    
-    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
 // 지정된 데이터 항목에서 HTML 목록 항목 생성
 function createHTMLString(item) {
-    return `
+  return `
     <li class="item">
             <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
             <span class="item__description">${item.gender}, ${item.size}</span>
@@ -23,29 +23,29 @@ function createHTMLString(item) {
 }
 
 function onButtonClick(event, items) {
-    const dataset = event.target.dataset;
-    const key = dataset.key;
-    const value = dataset.value;
-    
-    if(key === null || value === null) {
-        return;
-    }
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
 
-    const filtered = items.filter(item => item[key] === value);
-    displayItems(filtered);
+  if (key === null || value === null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
 }
 
 function setEventListers(items) {
-    const logo = document.querySelector('.logo');
-    const buttons = document.querySelector('.buttons');    
-    logo.addEventListener('click', () => displayItems(items));
-    buttons.addEventListener('click', evnet => onButtonClick(evnet, items));
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (evnet) => onButtonClick(evnet, items));
 }
 
 // main
 loadItems()
-.then(items => {    
+  .then((items) => {
     displayItems(items);
     setEventListers(items);
-})
-.catch(console.log);
+  })
+  .catch(console.log);
